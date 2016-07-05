8a26e17e01111d14a0c2e81dde8377aab539946a
Antoine07 <antoine.lucsko@wanadoo.fr>
2016-07-05 14:47:25 +0200

    gulp js en place

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     minify = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
     rename = require('gulp-rename')
-    browserSync = require('browser-sync');
+browserSync = require('browser-sync');
 
-gulp.task('hello', function(){
+gulp.task('hello', function () {
     console.log('hello les dev');
 });
 
 
-gulp.task('browserSync', function() {
+gulp.task('browserSync', function () {
     browserSync({
         proxy: 'localhost:8000'
     })
@@ -18,12 +19,15 @@ gulp.task('browserSync', function() {
 
 var path = {
     'resources': {
-        'sass': './resources/assets/sass'
+        'sass': './resources/assets/sass',
+        'js': './resources/assets/js'
     },
     'public': {
-        'css': './public/assets/css'
+        'css': './public/assets/css',
+        'js': './public/assets/js'
     },
-    'sass': './resources/assets/sass/**/*.scss'
+    'sass': './resources/assets/sass/**/*.scss',
+    'js': './resources/assets/js/**/*.js'
 };
 
 gulp.task('task-sass', function () {
@@ -38,12 +42,21 @@ gulp.task('task-sass', function () {
         .pipe(browserSync.reload({
             stream: true
         }))
+});
+
+gulp.task('task-js', function () {
+
+    return gulp.src(path.resources.js + '/app.js')
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(path.public.js));
 
 });
 
 // task lancé si une modification est faite dans les fichiers scss des resources/assets/sass
-gulp.task('watch',['browserSync', 'task-sass'], function () {
-    gulp.watch(path.sass, ['task-sass'])
+gulp.task('watch', function () {
+    gulp.watch(path.sass, ['task-sass']);
+    gulp.watch(path.js, ['task-js']);
 });
 
 gulp.task('default', ['watch']);
